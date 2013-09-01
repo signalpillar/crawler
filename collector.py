67c6efb488d1b83deb6018f8aabf046562f5205c
Vladimir Vitvitskiy <vladimir.vitvitskiy@hp.com>
2013-09-01 13:45:52 +0300

    working collector, single thread

--- collector.py
+++ collector.py
@@ -1,43 +1,117 @@
 from fn import F
 from collections import defaultdict, namedtuple
-from itertools import repeat, izip
+from itertools import repeat, izip, ifilterfalse
 import logging
+import requests
+import urlparse
+import re
 
 
-logging.basicConfig(levle=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
+SUPPORTED_MIME_TYPES = ('text/html',)
 
 UrlInfo = namedtuple("UrlInfo", ("incomming", "outgoing"))
 
 
+def new_url_info(incomming=None, outgoing=None):
+    '@types: set, set -> UrlInfo'
+    return UrlInfo(set(incomming or ()),
+                   set(outgoing or ()))
+
+
 def collect_outgoing_urls(start_url, limit):
-    info_by_url = defaultdict(F(UrlInfo, set(), set()))
-    parent_to_url_queue = [(None, start_url)]
+    '''
+    Algorithm applied below is very similar to BFS
+    '''
+    logger.info("staring url with limit %s: %s" % (limit, start_url))
+    info_by_url = defaultdict(new_url_info)
+    parent_to_url_queue = [(None, _normalize_url(start_url))]
     while parent_to_url_queue and limit > 0:
         parent_url, url = parent_to_url_queue.pop(0)
         if url not in info_by_url:
-            limit = limit - 1
-            outgoing_urls = get_page_urls(url)
+
+            is_page_reached, urls = _get_page_urls(url)
+            if is_page_reached:
+                limit = limit - 1
+                urls = ifilterfalse(_is_fragment_ref, urls)
+                urls = map(F(_normalize_url, url), urls)
+
+                info = info_by_url[url]
+                info.outgoing.update(urls)
+                if parent_url:
+                    info.incomming.add(parent_url)
+                candidates = urls
+                parent_to_url_queue.extend(izip(repeat(url), candidates))
+                print "OK     ", url, " <-- ", parent_url
+            else:
+                print "FAILED ", url, " <-- ", parent_url
+        else:
+            print "DONE   ", url, " <-- ", parent_url
             info = info_by_url[url]
-            info.outgoing.update(outgoing_urls)
-            if parent_url:
-                info.incomming.add(parent_url)
-            candidates = outgoing_urls[:limit]
-            parent_to_url_queue.extend(izip(repeat(url), candidates))
+            info.incomming.add(parent_url)
     return info_by_url
 
 
-def get_page_urls(url):
-    return []
+def _get_resource_head(url):
+    '''
+    Perform HEAD request to get headers by URL
+    @types: str -> tuple[int, str?]
+    '''
+    r = requests.head(url)
+    code, type_ = r.status_code, r.headers.get('content-type')
+    if type_:
+        # truncate charset information
+        type_ = type_.split(';', 1)[0]
+    logger.debug("HEAD: %s Code: %s, Content-Type: %s" % (url, code, type_))
+    return code, type_
 
 
-def parse_a_tag_urls(content):
-    pass
+def _get_resource_content(url):
+    '@types: str -> str?'
+    r = requests.get(url)
+    is_successful = r.status_code == requests.codes.ok
+    logger.debug("GET: %s, Success ?: %s" % (url, is_successful))
+    if is_successful:
+        return r.text
 
 
-def is_non_resource_url(url):
-    pass
+def _is_fragment_ref(url):
+    '@types: str -> bool'
+    return url.startswith("#")
 
 
-def normalize_url(url, uri):
-    pass
+def _get_page_urls(url):
+    ''' Get URLs on page by specified URL
+    @types: str -> bool, list[str]
+    @return: pair of flag whether page is reached at all
+             and parsed URLs from it'''
+    is_page_reached = False
+    urls = []
+    status_code, mime_type = _get_resource_head(url)
+    skip = mime_type and mime_type not in SUPPORTED_MIME_TYPES
+    if not skip:
+        content = _get_resource_content(url)
+        if content:
+            urls = _parse_a_tag_urls(content)
+            is_page_reached = True
+    return is_page_reached, urls
+
+
+A_TAG_HREF_RE = re.compile("<a\s+.*?href=\"(.*?)\"")
+
+
+def _parse_a_tag_urls(content):
+    '@types: str -> list[str]'
+    return set(A_TAG_HREF_RE.findall(content))
+
+
+def _normalize_url(url, uri=None):
+    '@types: str, str? -> str'
+    result = uri or url
+    if uri and not urlparse.urlparse(uri).netloc:
+        result = urlparse.urljoin(url, uri)
+    if not urlparse.urlparse(result).scheme:
+        result = "http://%s" % result
+    return result
--- tests/test_collector.py
+++ tests/test_collector.py
@@ -1,7 +1,236 @@
+# coding: utf-8
+from mock import patch
+
 import collector
+import contextlib
+
+from collector import _normalize_url, new_url_info
+
+from fn.iters import nth
 
 
-def test_simple_collection():
-    graph = collector.collect_outgoing_urls("google.com", 1)
-    print graph
-    assert graph == {"url1": "x"}
+def test_normalize_url():
+    base = "http://x.com"
+    uri = "/find"
+    assert "http://x.com/find" == _normalize_url(base, uri)
+    assert "http://x.com" == _normalize_url(base, base)
+
+    assert "http://x.com" == _normalize_url("x.com")
+
+
+def test_check_for_the_fragment_reference():
+    assert True == collector._is_fragment_ref("#header")
+    assert False == collector._is_fragment_ref("/find?#mark")
+    assert False == collector._is_fragment_ref("http://ty.ru/")
+
+
+def test_mimetype_guessing():
+    import mimetypes
+    actual = mimetypes.guess_type("http://at.com/file.exe")
+    assert ('application/x-msdownload', None) == actual
+
+    actual = mimetypes.guess_type("http://at.com/")
+    assert (None, None) == actual
+
+    actual = mimetypes.guess_type("http://at.com/index.html")
+    assert ("text/html", None) == actual
+
+
+def test_more_than_one_incomming():
+    # given
+    start_url = "http://today.sunday.in.ua/url1"
+
+    route_table = {
+        start_url:
+        (200, 'text/html', content_with_urls(
+            "http://first.com",
+            "http://gogo.go",
+            "http://greenlets.hm.let.me.try.us")),
+
+        "http://first.com":
+        (200, 'text/html', content_with_urls(
+            start_url,
+            "http://gogo.go")),
+
+        "http://gogo.go":
+        (200, 'text/html', content_with_urls(
+            start_url,
+            "http://from.go.go.go/index.jsp"))
+    }
+
+    # when
+    graph = __collect_links(start_url, route_table, 4)
+    # then
+    assert graph == {
+        start_url: new_url_info(
+            incomming=[
+                "http://first.com",
+                "http://gogo.go"],
+            outgoing=[
+                "http://first.com",
+                "http://gogo.go",
+                "http://greenlets.hm.let.me.try.us"]),
+
+        "http://first.com": new_url_info(
+            incomming=[start_url],
+            outgoing=[start_url,
+                      "http://gogo.go"]),
+
+        "http://gogo.go": new_url_info(
+            incomming=[
+                start_url,
+                "http://first.com"],
+            outgoing=[
+                start_url,
+                "http://from.go.go.go/index.jsp"])}
+
+
+def test_limit_value_influence_on_collecting():
+    # given
+    start_url = "http://today.sunday.in.ua/url1"
+    only_one = 1
+
+    route_table = {
+        start_url:
+        (200, 'text/html', content_with_urls(
+            "http://first.com",
+            "http://gogo.go",
+            "http://greenlets.hm.let.me.try.us")),
+
+        "http://first.com":
+        (200, 'text/html', content_with_urls(
+            start_url,
+            "http://gogo.go")),
+
+        "http://gogo.go":
+        (200, 'text/html', content_with_urls(
+            "http://from.go.go.go/index.jsp"))
+    }
+
+    # when
+    graph = __collect_links(start_url, route_table, only_one)
+    # then
+    assert graph == {
+        start_url: new_url_info(
+            incomming=[],
+            outgoing=[
+                "http://first.com",
+                "http://gogo.go",
+                "http://greenlets.hm.let.me.try.us"
+            ])}
+
+
+def test_cycle_references():
+    # given
+    start_url = "http://today.sunday.in.ua/url1"
+
+    route_table = {
+        start_url:
+        (200, 'text/html', content_with_urls(
+            "http://first.com")),
+
+        "http://first.com":
+        (200, 'text/html', content_with_urls(
+            start_url))
+    }
+
+    # when
+    graph = __collect_links(start_url, route_table, 11)
+
+    # then
+    assert graph == {start_url: new_url_info(incomming=["http://first.com"],
+                                             outgoing=["http://first.com"]),
+                     "http://first.com": new_url_info(incomming=[start_url],
+                                                      outgoing=[start_url])}
+
+
+def __test_general_flow_of_links_collecting():
+    start_url = "http://today.sunday.in.ua/url1"
+
+    route_table = {
+        start_url:
+        (200, 'text/html', content_with_urls(
+            "/url1",
+            "/",
+            "http://flavor",
+            "http://match.at.co",
+            "i.piont.to.first.com")),
+
+        "http://match.at.co":
+        (200, 'text/html', content_with_urls(
+            "nolinksatall.com/")),
+
+        "http://nolinksatall.com/":
+        (200, 'text/html', content_with_urls()),
+
+        "http://i.piont.to.first.com":
+        (200, 'text/html', content_with_urls(
+            start_url))
+    }
+
+    graph = __collect_links(start_url, route_table, 10)
+
+
+def __collect_links(start_url, route_table, visit_limit):
+    ''' Test how link collecting works in small sandbox
+
+    @types: str, dict[str, tuple[int, str, str]], int -> dict[str, UrlInfo]
+
+    @param route_table:
+        routing table defines small subset of WEB
+        that will be used for crawler testing
+        each record in table is composed of:
+        - URL to visit
+        - status code and mime type for HEAD request
+        - list of outgoing links included in visited page
+    '''
+
+    def get_content_from_route(url):
+        content = nth(route_table.get(url, ()), 2, default=None)
+        return content
+
+    def get_head_from_route(url):
+        r = route_table.get(url, (404, None, None))
+        status_code, mimetype, _ = r
+        return status_code, mimetype
+
+    with contextlib.nested(
+            patch("collector._get_resource_content", get_content_from_route),
+            patch("collector._get_resource_head", get_head_from_route)):
+        return collector.collect_outgoing_urls(start_url, visit_limit)
+
+
+def content_with_urls(*urls):
+    '''
+    Make sample http document which contains URLs
+    @types: list[str] -> str
+
+    >> content_with_urls("url_a", "url_b", "url_c")
+    <a href="url_a"></a>
+    <a href="url_b"></a>
+    <a href="url_c"></a>
+    '''
+    return "\n".join(
+        ('<a href="%s"></a>' % u for u in urls)
+    )
+
+
+def test_urls_parsed_from_html_content():
+    content = '''
+
+            <li>
+            <a href="http://stackexchange.com/sites#science" class="more">
+            more (7)
+            <li><a href="http://stackapps.com" >Stack Apps</a></li>
+            <li><a href="http://meta.stackoverflow.com"/> </a></li>
+            <li><a href="http://meta.stackoverflow.com"/> </a></li>
+            <li><a href="http://meta.stackoverflow.com"/> </a></li>
+            <li><a href="http://area51.stackexchange.com">Area 51</a></li>
+            <li><a href="http://careers.stackoverflow.com">Stackrs</a></li>
+                '''
+    expected = set(['http://stackexchange.com/sites#science',
+                    'http://stackapps.com',
+                    'http://meta.stackoverflow.com',
+                    'http://area51.stackexchange.com',
+                    'http://careers.stackoverflow.com'])
+    assert expected == collector._parse_a_tag_urls(content)
